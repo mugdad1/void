ae3462f1f3b6070c8f4a4f6b0ae0183c1eec3eae
mugdad <128133561+mugdad1@users.noreply.github.com>
2025-12-28 11:18:08 +0300

    Create void.bash

---
+++ void.bash
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Create and configure the repository file
+echo "repository=https://raw.githubusercontent.com/Encoded14/void-extra/repository-x86_64" | sudo tee /etc/xbps.d/20-repository-extra.conf
+
+# Update package database and install required packages
+sudo xbps-install -Syyuv \
+    hyprland \
+    hyprland-protocols \
+    xorg-server-xwayland \
+    xdg-desktop-portal-hyprland \
+    xdg-desktop-portal \
+    xdg-utils \
+    wayland \
+    wayland-protocols \
+    xdg-desktop-portal-wlr \
+    xdg-desktop-portal-gtk \
+    void-repo-multilib \
+    void-repo-nonfree \
+    wpa_supplicant \
+    wifish \
+    wpa-cute \
+    wpa_gui \
+    NetworkManager \
+    nmtui \
+    xorg \
+    gnome-keyring \
+    polkit-gnome \
+    mtpfs \
+    inotify-tools \
+    ffmpeg \
+    libnotify \
+    git \
+    base-devel \
+    pipewire \
+    wireplumber \
+    sof-firmware \
+    intel-media-driver
+
+# Notify user of completion
+echo "Installation completed successfully."
