f1c302eef049f1b8cac61a0b58d9e7d4a86fd073
mugdad <128133561+mugdad1@users.noreply.github.com>
2026-01-06 10:33:14 +0300

    Update void.sh

--- void.sh
+++ void.sh
@@ -2,21 +2,5 @@
 
 # Append configurations to .bash_profile
 
-cat <<EOL >> "$HOME/.bash_profile"
-
-# Load eww_scale
-[ -f "\$HOME/.eww_scale" ] && source "\$HOME/.eww_scale"
-
-# Ruby Path
-export GEM_HOME="\$(gem env user_gemhome)"
-export PATH="\$PATH:\$GEM_HOME/bin"
-
-# Mpd music dir, you also have to configure mpd...
-export MPD_MUSIC_DIR="\$HOME/where-musics-are"
-
-# Where Dotfiles are, if not ~/.dotfiles
-export DOTFILES_DIR="\$HOME/.dotfiles"
-
-EOL
-
-echo "Configurations added to .bash_profile. Please restart your terminal or run 'source ~/.bash_profile' to apply the changes."
+mkdir -p "$HOME/.config/sway"
+cp "$HOME/.dotfiles/swayfx/.config/sway/kdb.example" "$HOME/.config/sway/keyboard"
